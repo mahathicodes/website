815295d0dc1b18688018ef7d2f514beaa2e94571
Mahathi Gandhamaneni <86197077+mahathicodes@users.noreply.github.com>
2022-06-20 16:01:08 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,18 @@
 // Scripts
 // 
 
+var i = 0;
+var txt = 'Mahathi Gandhamaneni'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
